4c8db9c331670563a5ab8563d26643c524bb0301
Milan Grubnić <MilanG@aol.com>
2016-01-24 19:22:05 -0600

    refactored

--- jasmine/src/calculator.js
+++ jasmine/src/calculator.js
@@ -1,18 +1,18 @@
 var Calculator = function() {
 
     this.add = function(num1, num2) {
-        return num1 + num2;
+        return (num1 + num2);
     };
 
     this.sub = function(num1, num2) {
-        return num1 - num2;
+        return (num1 - num2);
     };
 
     this.multi = function(num1, num2) {
-        return num1 * num2;
+        return (num1 * num2);
     };
 
     this.div = function(num1, num2) {
-        return num1 / num2;
+        return (num1 / num2);
     };
 };
