3c731fee5b74601352bb6c2c8dc7a8b415262932
Milan Grubnić <MilanG@aol.com>
2016-01-24 19:16:17 -0600

    added sub method

--- jasmine/src/calculator.js
+++ jasmine/src/calculator.js
@@ -1,5 +1,10 @@
 var Calculator = function() {
+
     this.add = function(num1, num2) {
         return num1 + num2;
+    };
+
+    this.sub = function(num1, num2) {
+        return num1 - num2;
     };
 };
