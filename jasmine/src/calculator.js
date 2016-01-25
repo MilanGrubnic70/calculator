087f113285d20983dc0dbe23a5ba158f80f10877
Milan Grubnić <MilanG@aol.com>
2016-01-24 19:20:48 -0600

    debugged div spec

--- jasmine/src/calculator.js
+++ jasmine/src/calculator.js
@@ -11,4 +11,8 @@ var Calculator = function() {
     this.multi = function(num1, num2) {
         return num1 * num2;
     };
+
+    this.div = function(num1, num2) {
+        return num1 / num2;
+    };
 };
